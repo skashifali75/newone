4bcc649ded2e76abc7747645eda9cf9b2b3e0257
Syed Kashif Ali <64989679+skashifali75@users.noreply.github.com>
2020-12-26 18:53:33 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -9,13 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>
+          Kashif 
         </a>
       </header>
     </div>
