a05a032dae36cb1ec23d810b8afddc3bdfd383a7
Syed Kashif Ali <64989679+skashifali75@users.noreply.github.com>
2020-12-26 22:44:07 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>
-          Kashif 
-        </a>
-      </header>
+      Kashif 
     </div>
   );
 }
