d47463cb607bb3f6b2c09d928a4e53463279b09d
Syed Kashif Ali <64989679+skashifali75@users.noreply.github.com>
2020-12-27 19:38:58 +0000

    Create newcomp.js

---
+++ src/newcomp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Newcomp(){
+   return (
+     <div>
+         <h1>Its a line</h1>
+         <h2>It is another line</h2>
+     </div>
+
+   );
+}
+export default Newcomp;
